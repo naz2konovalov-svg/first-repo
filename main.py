ff8445f264325ae7cb2880e7eff2b1a2a4ef0bc2
Knvlv <naz2.konovalov@gmail.com>
2026-08-25 14:18:14 +0100

    1st test of  Collections

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-ім_я = input("Введіть ваше ім'я: ")
-вітання = f"Привіт, {ім_я}!"
-print(вітання)
+inventory = ["кирка", "земля", "алмаз", "земля", "дерево"]
+inventory.append("меч")
+print(f"Весь инвентарь: {inventory}")
+
+unique_items = set(inventory)
+print(f"Уникальные ресурсы: {unique_items}")
+
+hotbar = inventory[0:3]
+print(f"Быстрый доступ: {hotbar}")
+
+player_stats = {
+    "name": "Nazar",
+    "level": 16,
+    "health": 100
+}
+
+weapon = player_stats.get("weapon", "Оружие не найдено")
+print(f"Статус оружия: {weapon}")
+
+chat_message = "   ВНИМАНИЕ! КРИПЕР СЗАДИ!   "
+
+clean_message = chat_message.strip().lower()
+print(f"Чат гильдии: {clean_message}")
