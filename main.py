9864d109b5bb9e4260af272764ce3950c797c40f
Knvlv <naz2.konovalov@gmail.com>
2026-09-16 21:09:54 +0100

    loops and conditions try except

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-visited_cities = ["Seville", "Malaga", "Zagreb", "Malaga"]
+prices = [120, 45, 300, 15, 80]
+for index, item in enumerate(prices):
+    if item <= 50:
+        continue
+    if item >= 100:
+        new_price = item - 20
+        print(f"Discount price: {new_price}")
+    else:
+        print(f"Normal price: {item}")
 
-unique_cities = set(visited_cities)
-print (f"Visited cities: {unique_cities}")
 
-zagreb_expenses = {
-    "food": 10,
-    "transport": 20,
-    "souvenirs": 20
-}
-
-user_request = input("Enter expense category: ")
-clean_user_request = user_request.strip().lower()
-
-expense_ammount = zagreb_expenses.get(clean_user_request, "No data")
-print (f"Amount for {clean_user_request}: {expense_ammount}")
+while True:
+    user_input = input("Enter your age: ")
+    try:
+        user_input = int(user_input)
+    except ValueError:
+        print("Numbers only!")
+        continue
+    if user_input >= 120:
+        print("Write your real age please!")
+        continue 
+    if user_input < 18:
+        print("Too young!")
+        break
+    else:
+        print("Success! Welcome!")
+        break
